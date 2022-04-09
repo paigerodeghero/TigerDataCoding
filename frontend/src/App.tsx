49dc67a06007e6aff9e455a91d4e16828b3654e8
Kita Pairojtanachai (Chris) <kita.pairojtanachai@gmail.com>
2022-04-09 09:59:36 -0400

    added routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import ResetPassword from './components/ResetPassword';
+import Home from './components/Home';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				{/* <NavBar /> */}
+				<div id="wrapper-content">
+					<ScrollToTop />
+					<Routes>
+						<Route path="/login" element={<Login />} />
+						<Route path="/signup" element={<Signup />} />
+						<Route path="/reset-password" element={<ResetPassword />} />
+
+						<Route path="/" element={<Home />} />
+					</Routes>
+				</div>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,5 @@
+function Home() {
+	return <div></div>;
+}
+
+export default Home;
