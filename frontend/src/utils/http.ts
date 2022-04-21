2a98741d78010c67a939708481f6cb40d94bb1ad
Kita Pairojtanachai (Chris) <kita.pairojtanachai@gmail.com>
2022-04-21 15:30:33 -0400

    wip: axios interface

--- frontend/src/utils/http.ts
+++ frontend/src/utils/http.ts
@@ -3,49 +3,52 @@ import axios from 'axios';
 const instance = axios.create({ baseURL: process.env.REACT_APP_API_BASE_URL });
 
 instance.interceptors.request.use(
-  function (config) {
-    config.withCredentials = true;
+	function (config) {
+		const accessToken = localStorage.getItem('accessToken');
+		if (config.headers) {
+			config.headers['Authorization'] = 'Bearer ' + accessToken;
+		}
 
-    return config;
-  },
-  function (error) {
-    return Promise.reject(error);
-  }
+		return config;
+	},
+	function (error) {
+		return Promise.reject(error);
+	}
 );
 
 instance.interceptors.response.use(
-  (resp) => {
-    return resp;
-  },
-  (err) => {
-    let resp = err.response;
-    if (!resp) {
-      localStorage.clear();
+	(resp) => {
+		return resp;
+	},
+	(err) => {
+		let resp = err.response;
+		if (!resp) {
+			localStorage.clear();
 
-      if (window.location.pathname !== '/login') {
-        window.location.pathname = '/login';
-        alert(
-          'Unable to connect to the server at this moment. Please try again.'
-        );
-      }
-    } else if (resp.status === 401 && window.location.pathname !== '/login') {
-      window.location.pathname = '/login';
-    } else if (resp.status === 403) {
-      if (resp.data) alert(resp.data);
-      else alert('Permission Denied!');
+			if (window.location.pathname !== '/login') {
+				window.location.pathname = '/login';
+				alert(
+					'Unable to connect to the server at this moment. Please try again.'
+				);
+			}
+		} else if (resp.status === 401 && window.location.pathname !== '/login') {
+			window.location.pathname = '/login';
+		} else if (resp.status === 403) {
+			if (resp.data) alert(resp.data);
+			else alert('Permission Denied!');
 
-      if (window.location.pathname !== '/register') {
-        window.location.pathname = '/';
-      }
-    }
+			if (window.location.pathname !== '/register') {
+				window.location.pathname = '/';
+			}
+		}
 
-    return Promise.reject(err);
-  }
+		return Promise.reject(err);
+	}
 );
 
 const http = {
-  get: instance.get,
-  post: instance.post,
+	get: instance.get,
+	post: instance.post,
 };
 
 export default http;
