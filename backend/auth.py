0b89551bee89d8dd4681359d37d2709fd08b6df9
Sangamesh Itagi <itagi.sangamesh@gmail.com>
2022-04-26 13:56:43 -0400

    Token auth annotation

--- backend/auth.py
+++ backend/auth.py
@@ -54,3 +54,24 @@ def signout():
     logout_user()
 
     return redirect(url_for('login'))
+
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message': 'a valid token is missing'})
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            current_user = Users.query.filter_by(email=data['email']).first()
+        except:
+            return jsonify({'message': 'token is invalid'})
+
+        return f(current_user, *args, **kwargs)
+    return decorator
